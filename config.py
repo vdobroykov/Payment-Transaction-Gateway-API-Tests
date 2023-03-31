d05cc26414643063e83a3ecadf94470e8d9d1891
YOUR NAME <YOUR@EMAIL.com>
2023-03-31 23:12:52 +0300

    extend config

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 config = {
     "authorization": {
-        "username": "codemonster",
-        "password": "my5ecret-key2o2o",
         "authorizationType": "Basic",
         "authorizationToken": "Y29kZW1vbnN0ZXI6bXk1ZWNyZXQta2V5Mm8ybw=="
     },
